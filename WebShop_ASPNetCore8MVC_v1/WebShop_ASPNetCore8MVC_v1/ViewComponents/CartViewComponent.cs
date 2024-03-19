711695517db69ab2fcb59c1d293e84fe660766fe
TThienPhong <thaithienphong130@gmaim.com>
2024-03-19 17:03:52 +0700

    C013.1 Thông tin Cart

--- WebShop_ASPNetCore8MVC_v1/WebShop_ASPNetCore8MVC_v1/ViewComponents/CartViewComponent.cs
+++ WebShop_ASPNetCore8MVC_v1/WebShop_ASPNetCore8MVC_v1/ViewComponents/CartViewComponent.cs
@@ -14,10 +14,10 @@ namespace WebShop_ASPNetCore8MVC_v1.ViewComponents
             cart= HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();
         }
         */
-
+        public List<CartItem> cart => HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();
         public IViewComponentResult Invoke()
         {
-            var cart = HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();
+            /*var cart = HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();*/
 
             return View("CartPanel", new CartModel
             {
