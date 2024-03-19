007d8aa38e5dd5af0f2eb0f928966d21f47f2200
TThienPhong <thaithienphong130@gmaim.com>
2024-03-19 21:37:23 +0700

    C014 Dinh nghia RegisterVM

---
+++ WebShop_ASPNetCore8MVC_v1/WebShop_ASPNetCore8MVC_v1/ViewModels/RegisterVM.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShop_ASPNetCore8MVC_v1.ViewModels
+{
+    public class RegisterVM
+    {
+        [Key]
+        [Display(Name = "Tên đăng nhập")]
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
+        [MaxLength(20, ErrorMessage = "Tối đa 20 kí tự")]
+        public string MaKh { get; set; }
+
+
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [DataType(DataType.Password)]
+        public string MatKhau { get; set; }
+
+        [Display(Name = "Họ tên")]
+        [Required(ErrorMessage = "Vui lòng nhập họ tên")]
+        [MaxLength(50, ErrorMessage = "Tối đa 50 kí tự")]
+        public string HoTen { get; set; }
+
+        [Display(Name = "Giới tính")]
+        [Required(ErrorMessage = "Vui lòng chọn giới tính")]
+        public bool GioiTinh { get; set; } = true;
+
+        [Display(Name = "Ngày sinh")]
+        [DataType(DataType.Date)]
+        public DateTime? NgaySinh { get; set; }
+
+        [Display(Name = "Địa chỉ")]
+        [MaxLength(60, ErrorMessage = "Tối đa 60 kí tự")]
+        public string DiaChi { get; set; }
+
+        [Display(Name = "Điện thoại")]
+        [MaxLength(24, ErrorMessage = "Tối đa 24 kí tự")]
+        [RegularExpression(@"0[9875]\d{8}", ErrorMessage = "Chưa đúng định dạng di động Việt Nam")]
+        public string DienThoai { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Email")]
+        [EmailAddress(ErrorMessage = "Chưa đúng định dạng email")]
+        public string Email { get; set; }
+
+        public string? Hinh { get; set; }
+    }
+}
