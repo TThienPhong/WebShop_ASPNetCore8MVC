7b9a9ba86116b97f9e33e70531d935080d78620d
TThienPhong <thaithienphong130@gmaim.com>
2024-03-20 22:31:56 +0700

    C015 Tao form dang ký

---
+++ WebShop_ASPNetCore8MVC_v1/WebShop_ASPNetCore8MVC_v1/Controllers/KhachHangController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShop_ASPNetCore8MVC_v1.ViewModels;
+
+namespace WebShop_ASPNetCore8MVC_v1.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DangKy()
+        {
+
+            return View();
+        }
+
+		[HttpPost]
+		public IActionResult DangKy(RegisterVM model, IFormFile Hinh)
+		{
+
+			return View();
+		}
+	}
+}
